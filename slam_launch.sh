b44d31af76d90acf9467dc8f473d43bf0b81a303
miro <miroslav.jascur@tuke.sk>
2020-03-19 17:48:05 +0100

    modified:   slam_launch.sh

--- slam_launch.sh
+++ slam_launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source ../devel/setup.bash
 
-BAGPATH='/media/miro/DataStore/bags/OS1-16_city1.bag' #own path
-CONFIGPATH='/media/miro/DataStore/bags/sample_config_file_16.json' #own path
+BAGPATH='.../OS1-16_city1.bag' #own path
+CONFIGPATH='.../sample_config_file_16.json' #own path
 
-CARTOCONF='/home/miro/Documents/ros/github_ws/src/ouster_slam/' #own path
+CARTOCONF='.../src/ouster_slam/' #own path
 
 roslaunch ouster_slam os_slam.launch \
                              udp_hostname:=127.0.0.1 \
